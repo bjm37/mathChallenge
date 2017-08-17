5c0f55d1c4ca590a37f5fac2867171e2b9afe2a3
bjm37 <bjm37@calvin.edu>
2017-08-17 01:11:27 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace mathChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int low = 0;
+            int high = 3;
+
+            Console.WriteLine("The lowest she can go is " + low + "and the highest she can go is " + high);
+        }
+    }
+}
