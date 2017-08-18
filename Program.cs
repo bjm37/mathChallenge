c050c69eb77b2ab2cc8950927d3880ad36a0c193
bjm37 <bjm37@calvin.edu>
2017-08-18 14:17:28 -0400

    git status

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ namespace mathChallenge
     {
         static void Main(string[] args)
         {
-            int low = 0;
-            int high = 3;
+            Console.WriteLine("Welcome to the math challenge! Please type any four-digit number below:");
+            var number1 = Console.ReadLine();
 
-            Console.WriteLine("The lowest she can go is " + low + "and the highest she can go is " + high);
+            Console.WriteLine("Thanks! Now, please type a second four-digit number of choice below:");
+            var number2 = Console.ReadLine();
+
+            Convert.ToInt32(number1);
+            Convert.ToInt32(number2);
+
+            //perform the task with a new method. This will include converting the strings to integers
         }
     }
 }
