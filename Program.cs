e27d6b3905a749d87b04b709880a0bcd26c427fe
bjm37 <bjm37@calvin.edu>
2017-08-19 14:03:13 -0400

    copying from master

--- Program.cs
+++ Program.cs
@@ -8,28 +8,23 @@ namespace mathChallenge
     {
         static void Main(string[] args)
         {
-            //request the first number of the user
-            Console.WriteLine("Welcome to the math challenge! Please type any four-digit number below:");
-            String numOneInput = Console.ReadLine();
+            Console.WriteLine("Welcome to the math challenge! Please type any four-digit number below:");  //request the first number of the user
+            String firstInput = Console.ReadLine();
 
-            //integer validation
-            while (!Int32.TryParse(numOneInput, out int numberOne))
+            while (!Int32.TryParse(firstInput, out int num1))  //integer validation
             {
                 Console.WriteLine("Not a valid four-digit number, try again.");
-
-                numOneInput = Console.ReadLine();
+                firstInput = Console.ReadLine();
             }
 
-            //request the second number of the user
-            Console.WriteLine("Thanks! Now, please type a second four-digit number of choice below:");
-            String numTwoInput = Console.ReadLine();
 
-            //integer validation
-            while (!Int32.TryParse(numTwoInput, out int numberTwo))
+            Console.WriteLine("Thanks! Now, please type a second four-digit number of choice below:");  //request the second number of the user
+            String secondInput = Console.ReadLine();
+
+            while (!Int32.TryParse(secondInput, out int num2)) //integer validation
             {
                 Console.WriteLine("Not a valid four-digit number, try again.");
-
-                numTwoInput = Console.ReadLine();
+                secondInput = Console.ReadLine();
             }
         }
         //declaring task method
