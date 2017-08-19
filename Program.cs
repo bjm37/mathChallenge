f4c7190f5bbcc707bd1eb9b281037ac11abe85ff
bjm37 <bjm37@calvin.edu>
2017-08-19 10:47:14 -0400

    Adding notes to my integer validation code

--- Program.cs
+++ Program.cs
@@ -6,27 +6,28 @@ namespace mathChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the math challenge! Please type any four-digit number below:"); //request the first number of the user
-
+            //request the first number of the user
+            Console.WriteLine("Welcome to the math challenge! Please type any four-digit number below:"); 
             int numberOne;
-
             String numOneInput = Console.ReadLine();
-
+           
+            //integer validation
             while (!Int32.TryParse(numOneInput, out numberOne))
             {
-                Console.WriteLine("Not a valid number, try again.");
+                Console.WriteLine("Not a valid four-digit number, try again.");
 
                 numOneInput = Console.ReadLine();
             }
 
-            Console.WriteLine("Thanks! Now, please type a second four-digit number of choice below:"); //request the second number of the user
+            //request the second number of the user
+            Console.WriteLine("Thanks! Now, please type a second four-digit number of choice below:"); 
             int numberTwo;
-
             String numTwoInput = Console.ReadLine();
-
+            
+            //integer validation
             while (!Int32.TryParse(numTwoInput, out numberTwo))
             {
-                Console.WriteLine("Not a valid number, try again.");
+                Console.WriteLine("Not a valid four-digit number, try again.");
 
                 numTwoInput = Console.ReadLine();
             }
