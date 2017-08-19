472b605f27b6dc473f062b5816858cf880d9b259
bjm37 <bjm37@calvin.edu>
2017-08-19 12:31:45 -0400

    Something wrong with first Main method?

--- Program.cs
+++ Program.cs
@@ -1,21 +1,17 @@
-﻿using System;
+﻿using System;using System.Collections.Generic;
 
 namespace mathChallenge
 {
     class Program
     {
-        string numOneInput, numTwoInput;
-        int numberOne, numberTwo;
-
         public void Main(string[] args)
         {
             //request the first number of the user
             Console.WriteLine("Welcome to the math challenge! Please type any four-digit number below:");
-            int numberOne;
             String numOneInput = Console.ReadLine();
 
             //integer validation
-            while (!Int32.TryParse(numOneInput, out numberOne))
+            while (!Int32.TryParse(numOneInput, out int numberOne))
             {
                 Console.WriteLine("Not a valid four-digit number, try again.");
 
@@ -24,11 +20,10 @@ namespace mathChallenge
 
             //request the second number of the user
             Console.WriteLine("Thanks! Now, please type a second four-digit number of choice below:");
-            int numberTwo;
             String numTwoInput = Console.ReadLine();
 
             //integer validation
-            while (!Int32.TryParse(numTwoInput, out numberTwo))
+            while (!Int32.TryParse(numTwoInput, out int numberTwo))
             {
                 Console.WriteLine("Not a valid four-digit number, try again.");
 
@@ -36,6 +31,15 @@ namespace mathChallenge
             }
         }
         //declaring task method
+        public int[] NumbersIn(int numberOne)
+        {
+            var firstArray = new Stack<int>();
+
+            for (; numberOne > 0; numberOne /= 10)
+                firstArray.Push(numberOne % 10);
+
+            return firstArray.ToArray();
+        }
         /* public Stack<int> NumbersIn(int numberOne)
          {
              if (numberOne == 0) return new Stack<int>();
