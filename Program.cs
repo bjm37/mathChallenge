b0b1e86084e7a4191be437cfe437b5f8d29c9bda
bjm37 <bjm37@calvin.edu>
2017-08-19 14:16:11 -0400

    Trying to figure out methods

--- Program.cs
+++ Program.cs
@@ -10,8 +10,9 @@ namespace mathChallenge
         {
             Console.WriteLine("Welcome to the math challenge! Please type any four-digit number below:");  //request the first number of the user
             String firstInput = Console.ReadLine();
+            Int32 num1 = Convert.ToInt32(firstInput);
 
-            while (!Int32.TryParse(firstInput, out int num1))  //integer validation
+            while (!Int32.TryParse(firstInput, out int secondOutput))  //integer validation
             {
                 Console.WriteLine("Not a valid four-digit number, try again.");
                 firstInput = Console.ReadLine();
@@ -20,22 +21,31 @@ namespace mathChallenge
 
             Console.WriteLine("Thanks! Now, please type a second four-digit number of choice below:");  //request the second number of the user
             String secondInput = Console.ReadLine();
+            Int32 num2 = Convert.ToInt32(secondInput);
 
-            while (!Int32.TryParse(secondInput, out int num2)) //integer validation
+            while (!Int32.TryParse(secondInput, out int secondOutput)) //integer validation
             {
                 Console.WriteLine("Not a valid four-digit number, try again.");
                 secondInput = Console.ReadLine();
             }
+
         }
         //declaring task method
-        public int[] NumbersIn(int numberOne)
+        public int[] NumbersIn(int num1, int num2)
         {
-            var firstArray = new Stack<int>();
+            var firstArray = new Stack<int>(); //creating an array for the first number
 
-            for (; numberOne > 0; numberOne /= 10)
-                firstArray.Push(numberOne % 10);
+            for (; num1 > 0; num2 /= 10)
+                firstArray.Push(num1 % 10);
 
             return firstArray.ToArray();
+
+            var secondArray = new Stack<int>(); ////creating an array for the second number
+
+            for (; num1 > 0; num2 /= 10)
+                secondArray.Push(num1 % 10);
+
+            return secondArray.ToArray();
         }
         /* public Stack<int> NumbersIn(int numberOne)
          {
