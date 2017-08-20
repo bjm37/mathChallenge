c1f35136cff3e282e22945f54855d0d6619422e8
bjm37 <bjm37@calvin.edu>
2017-08-20 13:56:12 -0400

    Writing task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System;
 
 
 namespace mathChallenge
@@ -7,60 +7,45 @@ namespace mathChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the math challenge! Please type any four-digit number below:");  //request the first number of the user
+            Console.WriteLine("Welcome to the math challenge! Please type any three-digit number below:");  //request the first number of the user
             String firstInput = Console.ReadLine();
             Int32 num1 = Convert.ToInt32(firstInput);
 
-            while (!Int32.TryParse(firstInput, out int secondOutput))  //integer validation
+            while (!Int32.TryParse(firstInput, out int firstOutput))  //integer validation
             {
-                Console.WriteLine("Not a valid four-digit number, try again.");
-                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Not a valid three-digit number, try again.");
             }
 
-
-            Console.WriteLine("Thanks! Now, please type a second four-digit number of choice below:");  //request the second number of the user
+            Console.WriteLine("Thanks! Now, please type a second three-digit number of choice below:");  //request the second number of the user
             String secondInput = Console.ReadLine();
             Int32 num2 = Convert.ToInt32(secondInput);
 
             while (!Int32.TryParse(secondInput, out int secondOutput)) //integer validation
             {
-                Console.WriteLine("Not a valid four-digit number, try again.");
-                num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Not a valid three-digit number, try again.");
             }
 
-            int[] firstArray = new int[4];  //creating an array for the first number
-            for (; num1 > 0; num1 /= 10)
-                firstArray.(num1 % 10);
-            Console.WriteLine(firstArray);
+            // assigning each digit in the six slots a variable
+            int dig1num1 = 0;
+            int dig1num2 = 0;
+            int dig1num3 = 0;
 
-            int[] secondArray = new int[4]; //creating an array for the second number
-            for (; num2 > 0; num2 /= 10)
-                secondArray.(num2 % 10);
-            Console.WriteLine(secondArray);
+            int dig2num1 = 0;
+            int dig2num2 = 0;
+            int dig2num3 = 0;
 
-            /* public Stack<int> NumbersIn(int numberOne) Start a new method???*/
-            {
-                int total1 = 0;
-                int total2 = 0;
+            //Splitting each number into individual integers
+            dig1num1 = (num1 % 10);
+            dig1num2 = (num1 / 10);
+            dig1num3 = (num1 / 100);
 
-                for (i in firstArray; i < firstArray.length; i++)
-                {
-                    total1 += i;
-                }
-                for (i in secondArray; i < secondArray.length; i++)
-                {
-                    total2 += i;
-                }
+            dig2num1 = (num1 % 10);
+            dig2num2 = (num1 / 10);
+            dig2num3 = (num1 / 100);
 
-                if (total1 == total2)
-                {
-                    return true;
-                    else 
-                    {
-                        return false;
-                    }
-                }
-            }
+
+
+
 
         }
     }
